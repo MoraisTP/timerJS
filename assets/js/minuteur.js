fd535198249d4999fba37bc5635ac7da29bc0f9e
MoraisTP <morais05400@hotmail.com>
2025-02-18 13:34:47 +0100

    fix : Correction sur le window.onload + ajout de la fonction converti qui évite de répéter le code.

--- assets/js/minuteur.js
+++ assets/js/minuteur.js
@@ -1,10 +1,24 @@
-//Mes variable, je récup display (00:00..), le timer est null, je recup l'input gr^ce aux ids heures, minutes, secondes, millisecondes puis je crée une nouvelle date avec, je stock cette donnée sur un localStockage
+//Mes variable, je récup display (00:00..), le minuteur est null, je recup l'input gr^ce aux ids heures, minutes, secondes, millisecondes puis je crée une nouvelle date avec, je stock cette donnée sur un localStockage
 const display = document.getElementById('display');
-let timer = null;
+let minuteur = null;
 let recupInput = 0;
 let enCours = false;
 let tempsRestant = 0
+// La fonction minuteur permet (grâce à mathfloor) d'avoir les heures, minutes et secondes'
+function converti(difference) {
+    let heures = Math.floor(difference / (1000 * 60 * 60));
+    let minutes = Math.floor((difference / (1000 * 60)) % 60);
+    let secondes = Math.floor((difference / 1000) % 60);
+    let millisecondes = Math.floor((difference % 1000) / 10);
 
+    heures = String(heures).padStart(2, '0');
+    minutes = String(minutes).padStart(2, '0');
+    secondes = String(secondes).padStart(2, '0');
+    millisecondes = String(millisecondes).padStart(2, '0');
+
+    display.textContent = `${heures}:${minutes}:${secondes}:${millisecondes}`;
+}
+// La fonction start vérifie si le compteur est en cours et dans le premier cas (temps restant >0), il relance puis en deuxième (else), il lance le compteur de base
 function start() {
     if (!enCours) {
         if (tempsRestant > 0) {
@@ -18,74 +32,65 @@ function start() {
         recupInput = new Date().getTime() + (heures * 60 * 60 * 1000) + (minutes * 60 * 1000) + (secondes * 1000) + millisecondes;
     }
         localStorage.setItem('recupInput', recupInput);
-        localStorage.removeItem('tempsRestant');
+        localStorage.setItem('tempsRestant', recupInput - new Date().getTime());
+        localStorage.setItem('etat', 'running');
 
-        timer = setInterval(update, 10);
+        clearInterval(minuteur);
+        minuteur = setInterval(update, 10);
         enCours = true;
     }
 }
-// fonct qui permet de stop le minuteur
+// fonct qui permet de stop le minuteur, on save avec loclstorage
 function stop() {
     if (enCours) {
-        clearInterval(timer);
+        clearInterval(minuteur);
         enCours = false;
 
-        const now = new Date().getTime();
-        tempsRestant = recupInput - now;
+        tempsRestant = recupInput - new Date().getTime();
         localStorage.setItem('tempsRestant', tempsRestant);
+        localStorage.setItem('etat', 'stop');
     }
 
 }
-// fonct qui reset le minuteur
+// fonct qui reset le minuteur, on clearIntervl et on remet le tout à 0 + on clear le local storage
 function reset() {
-    clearInterval(timer);
+    clearInterval(minuteur);
     recupInput = 0;
     tempsRestant = 0;
     enCours = false;
     display.textContent = '00:00:00:00';
-    localStorage.removeItem('recupInput');
-    localStorage.removeItem('tempsRestant');
+    localStorage.clear();
 }
 
 //fonct qui update le temps
 function update() {
-    const now = new Date().getTime();
-    const difference = recupInput - now;
+
+    const maintenant = new Date().getTime();
+    const difference = recupInput - maintenant;
 
     // Si le temps est écoulé j'ffiche un messge
     if (difference < 0) {
         display.textContent = "Terminé !";
-        clearInterval(timer);
+        clearInterval(minuteur);
         enCours = false;
-        localStorage.removeItem('recupInput');
-        localStorage.removeItem('tempsRestant');
+        localStorage.clear();
         return;
     }
 
-    let heures = Math.floor(difference / (1000 * 60 * 60));
-    let minutes = Math.floor((difference / (1000 * 60)) % 60);
-    let secondes = Math.floor((difference / 1000) % 60);
-    let millisecondes = Math.floor((difference % 1000) / 10);
-
-    heures = String(heures).padStart(2, '0');
-    minutes = String(minutes).padStart(2, '0');
-    secondes = String(secondes).padStart(2, '0');
-    millisecondes = String(millisecondes).padStart(2, '0');
-
-    display.textContent = `${heures}:${minutes}:${secondes}:${millisecondes}`;
+    converti(difference)
 }
 //rechargement de la pge
 window.onload = function () {
     display.textContent = '00:00:00:00';
 
-    const saveRecupInput = localStorage.getItem('recupInput');
     const saveTempsRestant = localStorage.getItem('tempsRestant');
+    const etat = localStorage.getItem('etat');
 
-    if (saveRecupInput) {
-        recupInput = parseInt(saveRecupInput);
-        timer = setInterval(update, 10);
-        enCours = true;
-    } else if (saveTempsRestant) {
+
+    if (saveTempsRestant && etat === 'stop') {
         tempsRestant = parseInt(saveTempsRestant);
+        converti(tempsRestant);
+    } else if (etat === 'running') {
+        start()
     }
 };
